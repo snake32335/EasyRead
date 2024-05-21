642b353b55663793f25809659dfe1c02202c542d
Mexaak-hue <manjaroxfce4@gmail.com>
2024-05-21 12:48:54 +0400

    fix

--- source/script/script-lk-page.js
+++ source/script/script-lk-page.js
@@ -1,40 +1,16 @@
-let intervalid;
-
-document.querySelectorAll('.dropdown-toggle').forEach(e => {
-    e.addEventListener("click", e => {
-        const menu = e.currentTarget.dataset.path;
-        document.querySelectorAll('.dropdown-menu').forEach(e => {
-            if  (!(document.querySelector(`[data-target=${menu}]`).classList.contains('open'))){
-                e.classList.remove('menu-active');
-                e.classList.remove('open');
-                document.querySelector(`[data-target=${menu}]`).classList.add('menu-active')
-                setTimeout(() => {
-                    document.querySelector(`[data-target=${menu}]`).classList.add('open')  
-                }, 0)
-            }
-            if  (document.querySelector(`[data-target=${menu}]`).classList.contains('open')){
-                clearTimeout(intervalid)
-                document.querySelector(`[data-target=${menu}]`).classList.remove('menu-active')
-                setTimeout(() => {
-                    document.querySelector(`[data-target=${menu}]`).classList.remove('open')  
-                }, 0)
-            }
-        })
-    })
-})
-
-function show_list() {
-    var courses = document.getElementById("courses_id");
-
-    if (courses.style.display == "block") {
-        courses.style.display = "none";
+function show_hide() {
+    var click = document.getElementById("books");
+    if (click.style.display === "none") {
+       click.style.display = "block";
     } else {
-        courses.style.display = "block";
+       click.style.display = "none";
     }
-}
-window.onclick = function (event) {
-    if (!event.target.matches('.dropdown_button')) {
-        document.getElementById('courses_id')
-            .style.display = "none";
+ }
+ function show_hide() {
+    var click = document.getElementById("community");
+    if (click.style.display === "none") {
+       click.style.display = "block";
+    } else {
+       click.style.display = "none";
     }
-}   
+ }
