e4a1552a83f422f266a55e544e9c74377b4e6d83
Mexaak-hue <manjaroxfce4@gmail.com>
2024-05-23 13:56:01 +0400

    Корректировка(Титов)

---
+++ vue-project/src/App.vue
@@ -0,0 +1,34 @@
+
+<template>
+    <input type="text" @input="insertData($event.target.value)" placeholder="Имя">
+    <input type="password" placeholder="Пароль">
+    <input type="email" placeholder="Email">
+    <p>{{ userName }}</p>
+</template>
+
+<script>
+    export default{
+        data() {
+            return{
+                userName: "",
+                userPass: '',
+                userEmail: '',
+            }
+        },
+        methods: {
+            insertData(val) {
+                this.userName = val
+            }
+        }
+    }
+
+</script>
+
+<style scoped>
+h3{
+    font-weight: lighter;
+}
+p{
+    color: red;
+}
+</style>userName: ""userPass: ''userEmail: ''userName: ""userPass: ''userEmail: ''userPass: ''userEmail: ''
